ae8c1cf75919a4dc59ead71766533d17d248f669
apodaca <apodaca.gilbert@gmail.com>
2016-10-16 23:37:32 -0600

    commit

---
+++ compound_inequalities.py
@@ -0,0 +1,10 @@
+user_age = input('how old are you?:')
+
+if(user_age <=5):
+	print("your cost for enterence is $10")
+
+elif(user_age <=18)
+	  print("your cost for enterence is $25")
+
+else()
+		print("too old, man")
